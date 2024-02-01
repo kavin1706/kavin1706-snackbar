32aaf3032ca0435b0a4b7bf39e2253413c68f168
KAVIN1703 <kavinkumar1706@gmail.com>
2024-02-01 12:30:02 +0530

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -117,9 +117,11 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Incremented',
+
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+//sample code
